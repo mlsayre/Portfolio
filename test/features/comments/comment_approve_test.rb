cea7876cbdd7c08bc61c5d57c1577c19f4995492
Matt Sayre <mlsayre@gmail.com>
2013-10-23 13:31:06 -0700

    finished comments policy

--- test/features/comments/comment_approve_test.rb
+++ test/features/comments/comment_approve_test.rb
@@ -2,13 +2,15 @@ require "test_helper"
 
 feature "approving a comment" do
   scenario "editor role can approve any comment" do
+    # random visitor makes an idiotic comment
     new_comment
     sign_in(:editor)
-    visit post_path(posts(:cd).id)
     #save_and_open_page
 
     # Then the post should be changed
     page.text.must_include 'Approve this comment'
+
+    # TODO: Author can approve anyone's comment on their own post
 
   end
 end
